6a05f99b78634371031bb8cf086c3922b129a481
陈华狄 <861883474@qq.com>
2018-09-14 20:23:07 +0800

    rename

--- template/src/plugins/ddv-util/index.js
+++ template/src/plugins/ddv-util/index.js
@@ -1,9 +1,9 @@
 import api from 'ddv-restful-api-wxmini'
 import utilTime from 'ddv-util/time'
-import d from 'ddv-util'
+import utils from 'ddv-util'
 import ddvUtil from '@/assets/ddv-util'
 
-d.extendInit(utilTime, api.util)
-ddvUtil(d)
+utils.extendInit(utilTime, api.util)
+ddvUtil(utils)
 
-export default d
+export default utils
