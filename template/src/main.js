34ff6f450a124447cf7dbde173e85e48b9f8e597
陈华狄 <861883474@qq.com>
2018-09-14 16:40:52 +0800

    rename fo d

--- template/src/main.js
+++ template/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from '@/App'
 import store from '@/store'
-import d from '@/plugins/ddv-util'
+import ddvUtil from '@/plugins/ddv-util'
 import i18n from '@/plugins/i18n'
 import MpvueRouterPatch from 'mpvue-router-patch'
 
@@ -9,8 +9,7 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 Vue.use(MpvueRouterPatch)
-Vue.filter('d', d)
-Vue.prototype.$ddvUtil = d
+Vue.prototype.$ddvUtil = ddvUtil
 
 const app = new Vue({
   store,
