6af0790ec010a19130a3fa8f886a8009d8354a51
陈华狄 <861883474@qq.com>
2018-09-14 15:57:09 +0800

    修改

--- template/src/main.js
+++ template/src/main.js
@@ -6,13 +6,15 @@ import i18n from '@/plugins/i18n'
 import MpvueRouterPatch from 'mpvue-router-patch'
 
 Vue.config.productionTip = false
-App.store = store
-App.i18n = i18n
 App.mpType = 'app'
 
 Vue.use(MpvueRouterPatch)
 Vue.filter('d', d)
 Vue.prototype.$ddvUtil = d
 
-const app = new Vue(App)
+const app = new Vue({
+  store,
+  i18n,
+  ...App
+})
 app.$mount()
