26c073cfc98167d0432a1122fcdc6ee2e0128d12
陈华狄 <861883474@qq.com>
2018-09-19 16:31:45 +0800

    add ddv-i18n

---
+++ template/src/plugins/ddv-i18n/index.js
@@ -0,0 +1,7 @@
+import DdvI18n from './packages/main.vue'
+
+DdvI18n.install = function (Vue) {
+  Vue.component(DdvI18n.name, DdvI18n)
+}
+
+export default DdvI18n
---
+++ template/src/plugins/ddv-i18n/packages/main.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>{{word}}</span>
+</template>
+
+<script>
+export default {
+  name: 'DdvI18n',
+  props: {
+    type: {
+      type: String,
+      default: '$t',
+      validator (val) {
+        return ['$t', '$tc', '$te', '$d', '$n'].indexOf(val) > -1
+      }
+    },
+    value: [String, Number, Array, Object],
+    choice: [String, Number, Array, Object]
+  },
+  computed: {
+    word () {
+      return this[this.type](this.value, this.choice)
+    }
+  }
+}
+</script>
+
